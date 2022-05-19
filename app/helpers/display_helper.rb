79b1e04e05afcf8b68d40550f0690e3e8c36fafc
Evgeniy Denisov <lifeemulation@gmail.com>
2022-05-19 22:21:23 +0300

    Fix display_helper.rb

--- app/helpers/display_helper.rb
+++ app/helpers/display_helper.rb
@@ -1,15 +1,4 @@
 module DisplayHelper
-  DISPLAY_NAME_FALLBACK = -> {
-    name = ""
-    klass = self.class
-    name << klass.model_name.human if klass.respond_to? :model_name
-    name << " ##{send(klass.primary_key)}" if klass.respond_to? :primary_key
-    name.present? ? name : to_s
-  }
-  def DISPLAY_NAME_FALLBACK.inspect
-    "DISPLAY_NAME_FALLBACK"
-  end
-
   # DryCrud makes educated guesses when displaying objects, this is
   # the list of methods it tries calling in order
   DISPLAY_NAME_METHODS ||= [ :dry_crud_display_name,
@@ -23,10 +12,22 @@ module DisplayHelper
                              :to_s ].freeze
 
 
+  # Tries to compose display_name from resource
+  def display_name_fallback(resource)
+    name = ""
+    klass = resource.class
+    name << klass.model_name.human if klass.respond_to? :model_name
+    name << " ##{resource.send(klass.primary_key)}" if klass.respond_to? :primary_key
+    name.present? ? name : resource.to_s
+  end
+
   # Attempts to call any known display name methods on the resource.
   # See the setting in `application.rb` for the list of methods and their priority.
   def display_name(resource)
-    ERB::Util.html_escape(resource.send display_name_method_for(resource)) unless resource.nil?
+    return if resource.nil?
+    name_method = display_name_method_for(resource)
+    name = name_method ? resource.send(name_method) : display_name_fallback(resource)
+    ERB::Util.html_escape(name)
   end
 
   # Looks up and caches the first available display name method.
@@ -38,13 +39,11 @@ module DisplayHelper
       methods = DISPLAY_NAME_METHODS - association_methods_for(resource)
       method = methods.detect { |method| resource.respond_to? method }
 
-      if method != :to_s || resource.method(method).source_location
-        method
-      else
-        DISPLAY_NAME_FALLBACK
-      end
+      method if method != :to_s || resource.method(method).source_location
     end
+    nil
   end
+
 
   def association_methods_for(resource)
     return [] unless resource.class.respond_to? :reflect_on_all_associations
