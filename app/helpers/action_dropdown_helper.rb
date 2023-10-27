ded7e58a7ab02b75253a3e09240551f7aaacac56
dugandobbs <duganwdobbs@gmail.com>
2023-10-27 17:05:13 -0400

    Cleaning up module scope and some rubocop stuff

--- app/helpers/action_dropdown_helper.rb
+++ app/helpers/action_dropdown_helper.rb
@@ -1,4 +1,4 @@
-module DryCrudExtension::ActionsDropdownHelper
+module ActionsDropdownHelper
   include ActionsHelper
 
   def action_dropdown(label, icon = nil, options = [], html_options = {}, config = {})
@@ -8,11 +8,12 @@ module DryCrudExtension::ActionsDropdownHelper
 
   def action_dropdown_button(label, icon = nil, options = [], html_options = {}, btn_type: 'gold')
     content = icon ? action_icon(icon, label) : label
-    render 'shared/action_dropdown_button', content:, options:, html_options:, btn_type:
+    render 'shared/action_dropdown_button', content: content, options: options, html_options: html_options, btn_type: btn_type
   end
 
   def action_dropdown_checkbox(label, f, field, values_hash = [])
-    raise 'You need to send the form object to action_dropdown_checkbox.' unless f.present?
-    render 'shared/action_dropdown_checkbox', content: label, f:, field:, values_hash:
+    raise 'You need to send the form object to action_dropdown_checkbox.' if f.blank?
+
+    render 'shared/action_dropdown_checkbox', content: label, f: f, field: field, values_hash: values_hash
   end
 end
